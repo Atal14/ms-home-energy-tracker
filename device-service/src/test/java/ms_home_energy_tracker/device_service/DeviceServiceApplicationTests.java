0647327c41a7945b658b2ab7828066bfb1ea999c
Atal Sohail <atal@live.com>
2026-08-24 04:39:41 +0500

    Add test to add dummy data to DB

---
+++ device-service/src/test/java/ms_home_energy_tracker/device_service/DeviceServiceApplicationTests.java
@@ -0,0 +1,41 @@
+package ms_home_energy_tracker.device_service;
+
+import lombok.extern.slf4j.Slf4j;
+import ms_home_energy_tracker.device_service.entity.Device;
+import ms_home_energy_tracker.device_service.model.DeviceType;
+import ms_home_energy_tracker.device_service.repository.DeviceRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class DeviceServiceApplicationTests {
+
+	public static final int NUMBER_OF_DEVICES = 200;
+	public static final int USERS = 10;
+
+	@Autowired
+	private DeviceRepository deviceRepository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Disabled
+	@Test
+	void createDevice() {
+		for (int i = 1; i <= NUMBER_OF_DEVICES; i++) {
+			Device device = Device.builder()
+					.name("Device " + i)
+					.type(DeviceType.values()[i % DeviceType.values().length])
+					.location("Location" + ((i % 3) + 1))
+					.userId((long) ((i % USERS) + 1))
+					.build();
+			deviceRepository.save(device);
+		}
+		log.info("Device Repository has been populated");
+	}
+
+}
--- user-service/user-service/src/test/java/ms_home_energy_tracker/user_service/UserServiceApplicationTests.java
+++ user-service/user-service/src/test/java/ms_home_energy_tracker/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,41 @@
 package ms_home_energy_tracker.user_service;
 
+import lombok.extern.slf4j.Slf4j;
+import ms_home_energy_tracker.user_service.entity.User;
+import ms_home_energy_tracker.user_service.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	private static final int NUMBER_OF_USERS = 10;
+
+	@Autowired
+    UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Disabled
+	@Test
+	void addUsersToDB() {
+		for (int i = 1; i <= NUMBER_OF_USERS; i++) {
+			User user = User.builder()
+					.name("User" + i)
+					.surname("Surname" + i)
+					.email("user" + i + "@example.com")
+					.address(i + " Example St")
+					.alerting(i % 2 == 0)
+					.alertingThreshold(1000.0 + i)
+					.build();
+			userRepository.save(user);
+		}
+		log.info("User Repository populated successfully");
 	}
 
 }
